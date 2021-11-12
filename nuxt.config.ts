45429d5e5f835ab0015da467f145391d93ace058
Riadz <riad.hachemane@gmail.com>
2021-11-12 23:36:30 +0100

    added global sass variables + contaienr max width

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,14 @@ import { defineNuxtConfig } from 'nuxt3';
 
 export default defineNuxtConfig({
   css: ['@/assets/styles/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@import "@/assets/styles/primeflex/_variables.scss";',
+        },
+      },
+    },
+  },
 });
